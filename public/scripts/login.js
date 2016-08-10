c95f22287641b05a5fa6babf61d071cdf740cd3c
Aaron Bini <bini.aaron@gmail.com>
2016-08-10 08:31:52 -0700

    front-end user page

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -17,10 +17,10 @@
   };
 
   login.userOptions = function () {
-    console.log(tempToken);
-    console.log(loginUser);
+    // console.log(tempToken);
+    // console.log(loginUser);
     if(tempToken) {
-      $('#user-options').html(`<p>Current User: ${loginUser} <button id="logout">Log Out</button></p>`);
+      $('#user-options').html(`<p>Current User: ${loginUser} <button id="logout">Log Out</button> <button id="go-user">User Page</button> <button id="go-home" style="display: none;">Home Page</button></p>`);
     } else {
       $('#user-options').html('<button id="login-button">Log In</button> <button id="signup-button">Sign Up</button>');
     }
@@ -73,7 +73,7 @@
           toHtml('series', e, '#user-series');
         });
       });
-  };  
+  };
 
   login.startLogin();
   login.userOptions();
--- public/scripts/user.js
+++ public/scripts/user.js
@@ -2,20 +2,45 @@
 
   const user = {};
 
+  user.goToUserPage = function () {
+    $('#user-options').on('click', '#go-user', e => {
+      e.preventDefault();
+      let userId = Cookies.get('id');
+      $('#landing-page').hide();
+      $('#login-form').hide();
+      $('#go-home').show();
+      $('#go-user').hide();
+      $('#user-page').show();
+      $('#user-series').empty();
+      login.getSeries(userId);
+    });
+  };
+
+  user.goToLanding = function () {
+    $('#user-options').on('click', '#go-home', e => {
+      e.preventDefault();
+      $('#landing-page').show();
+      $('#go-home').hide();
+      $('#go-user').show();
+      $('#user-page').hide();
+    });
+  };
 
 
   user.getInstallments = function () {
     let userId = Cookies.get('id');
+    console.log(userId);
     $('#user-series').on('click', 'button[data-id]', (e) => {
       e.preventDefault();
       let location = '#' + $(e.target).data('id');
       console.log($(e.target).text());
       if($(e.target).text() === 'Show Installments') {
-        $(e.target).text('Hide Installments'); 
+        $(e.target).text('Hide Installments');
         superagent
           .get('api/installments/' + $(e.target).data('id') + '/approvals/' + userId)
           .then(result => {
             result.body.forEach(e => {
+              // e.releaseDate = moment(e.releaseDate).format('MM DD YYYY');
               if(e.summary[0] === 'You have not apporoved this installment for viewing.') {
                 e.notApproved = true;
               } else {
@@ -29,7 +54,7 @@
         $(location).empty();
         $(e.target).text('Show Installments');
       }
-    });   
+    });
   };
 
   user.manageApprovals = function() {
@@ -59,12 +84,14 @@
       // let location = '#' + $(e.target).id;
       if(e.target.value === 'delete') {
       }
-      
+
     });
-    
+
   };
 
   user.manageApprovals();
   user.getInstallments();
+  user.goToUserPage();
+  user.goToLanding();
   module.user = user;
 })(window);
